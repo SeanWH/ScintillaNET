459df70436d82c64afc3e02a4329e264ed0c3d57
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-02-20 19:01:44 +0000

    Move native dlls to runtime\win folders

--- src/ScintillaNET/Properties/AssemblyInfo.cs
+++ src/ScintillaNET/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f8ac48e7-9378-482d-8c7f-92c8408
+[assembly: Guid("f8ac48e7-9378-482d-8c7f-92c8408dd4f2")]
